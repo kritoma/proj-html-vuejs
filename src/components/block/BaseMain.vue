ce12a7e6784406d6b137d0df2b856a486d4cf386
kritoma <toma.kristian97@gmail.com>
2022-06-15 15:05:00 +0200

    aggiunto sezione play

--- src/components/block/BaseMain.vue
+++ src/components/block/BaseMain.vue
@@ -2,15 +2,17 @@
   <main>
     <SectionMusicLife/>
     <SectionBand/>
+    <SectionPlay/>
   </main>
 </template>
 
 <script>
 import SectionBand from '../section/SectionBand.vue';
 import SectionMusicLife from '../section/SectionMusicLife.vue';
+import SectionPlay from '../section/SectionPlay.vue';
 
 export default {
-  components: { SectionMusicLife, SectionBand },
+  components: { SectionMusicLife, SectionBand, SectionPlay },
     name: "BaseMain",
 }
 </script>
---
+++ src/components/section/SectionPlay.vue
@@ -0,0 +1,45 @@
+<template>
+  <section>
+    <div class="container">
+        <p>"I just listened to it and said to myself, "Man, I really love this album." Still, today, it just sounds so fresh. It sounds full of ideas. These guys knew what they were doing. They're good. And they're inventive. I haven't heard anything this year that's as inventive. i don't really expect to."</p>
+        <i class="fa-solid fa-play"></i>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+section {
+    background-image: url("../../assets/img/home-testimonial-parallax.jpg");
+    background-size: cover;
+    background-position: center;
+    padding: 100px 0;
+    color: white;
+    text-align: center;
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    
+    p {
+        width: 50%;
+    }
+
+    i{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        background-color: var(--color-primary);
+    }
+}
+</style>
