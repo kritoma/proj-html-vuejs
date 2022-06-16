bba6c6a08ff5e9abcaaf13b4ce128f7863e78196
kritoma <toma.kristian97@gmail.com>
2022-06-16 14:53:08 +0200

    ultimo commit

--- src/components/common/CardDates.vue
+++ src/components/common/CardDates.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="cardDates">
-    <i class="fa-solid fa-plus"></i>
-    <p>{{live.date}}, {{live.festival}}, {{live.country}}</p>
+  <div class="cards">
+    <div class="dates">
+        <i @click="openCurtain(live.visible)" class="fa-solid fa-plus"></i>
+        <p>{{live.date}}, {{live.festival}}, {{live.country}}</p>
+    </div>
+    <div class="maps" >
+        <img src="../../assets/img/maps.png" alt="maps">
+        <div class="text_maps" :class="{active : visible}">
+            <h4>Untold Stories</h4>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, dolorum ipsam, nihil possimus illum totam vitae cupiditate voluptatem autem vero praesentium fugiat incidunt saepe et nostrum, esse cum quas beatae!</p>
+            <button>BOOK NOW</button>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -10,18 +20,28 @@ export default {
     name: "CardDates",
     props: {
         live: Object,
+        i: Number
+    },
+    methods: {
+        openCurtain(index) {
+           index = !index
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
-.cardDates {
+.cards {
     background-color: #323844;
-    color: var(--color-primary);
-    display: flex;
-    align-items: center;
-    gap: 1.25rem;
-    padding: 10px;
+    
+
+    .dates {
+        color: var(--color-primary);
+        display: flex;
+        align-items: center;
+        gap: 1.25rem;
+        padding: 10px;
+    }
 
     p {
         margin: 0;
@@ -32,5 +52,47 @@ export default {
         font-size: 8px;
         margin-left: 1.25rem;
     }
+
+    
+    
+        
+        .text_maps {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            color: white;
+
+            h4 {
+                padding-top: 1.25rem;
+                font-size: 30px;
+            }
+
+            p {
+                color: #7d7b8d;
+            }
+
+            button {
+                background-color: var(--color-primary);
+                border: none;
+                color: white;
+                padding: 10px 0px;
+                width: 20%;
+                
+            }
+        }
+        
+
+        img {
+            height: 200px;
+        }
+    
 }
+.maps {
+        padding: 20px;
+        display: none;
+        gap: 4rem;
+}    
+.active {
+            display: flex;
+        }
 </style>
--- src/components/section/SectionLiveDates.vue
+++ src/components/section/SectionLiveDates.vue
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="live">
             <TitleSection title="Live Dates"/>
-            <CardDates class="list" v-for="(live, index) in liveDates" :key="index" :live="live"/>
+            <CardDates class="list" v-for="(live, index) in liveDates" :key="index" :live="live" :i="index"/>
         </div>
     </div>
     <DisplayBar text="VIEW ALL LIVES DATES"/>
@@ -26,26 +26,31 @@ export default {
                     date: "17/08/2020",
                     festival: "GEM FESTIVAL 2020 ANAKALIA",
                     country: "GEORGIA",
+                    visible: false,
                 },
                  {
                     date: "24/09/2020",
                     festival: "GROOVEFEST",
                     country: "DOMINICAL REPUBLIC",
+                    visible: false,
                 },
                  {
                     date: "31/10/2020",
                     festival: "OASIS FESTIVAL 2020 MARRAKECH",
                     country: "MOROCCO",
+                    visible: false,
                 },
                  {
                     date: "07/11/2020",
                     festival: "MOGA FESTIVAL - ESSAOURIA",
                     country: "MOROCCO",
+                    visible: false,
                 },
                  {
                     date: "10/12/2020",
                     festival: "ENVISION FESTIVAL - UVITA",
                     country: "COSTA RICA",
+                    visible: false,
                 },
             ],
         }
