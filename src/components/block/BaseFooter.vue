d0ff4a6e2c562b4d8ae3ef73965b259f0d242ac1
kritoma <toma.kristian97@gmail.com>
2022-06-16 10:34:15 +0200

    aggiustato footer

--- src/components/block/BaseFooter.vue
+++ src/components/block/BaseFooter.vue
@@ -1,6 +1,14 @@
 <template>
   <footer>
-    
+    <div class="copyrigth">
+      <span>© Copyrigth 2012 - 2020 | AVADA THEME BY THEMEFUSION | ALL RIGHTS RESERVED | POWERED BY WORDPRESS</span>
+    </div>
+    <div class="social">
+      <img src="../../assets/img/footer-facebook.png" alt="facebook">
+      <img src="../../assets/img/footer-twitter.png" alt="twitter">
+      <img src="../../assets/img/footer-pinterest.png" alt="pinterest">
+      <img src="../../assets/img/footer-youtube.png" alt="youtube">
+    </div>
   </footer>
 </template>
 
@@ -10,6 +18,18 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+footer {
+  background-color: #252a33;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.25rem;
+  color: #747384;
 
+  .social {
+    display: flex;
+    gap: .625rem;
+  }
+}
 </style>
